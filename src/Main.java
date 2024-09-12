e4eae0621331b7b5d3a85dbc109c2dfe21be6254
faximon <vitor.faximon@gmail.com>
2024-09-12 19:48:45 -0300

    Aula 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-public class Estudos {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hoje tive aula de Java!");
         System.out.println("Aprendemos sobre as formas que a JDK executa em diferentes sistemas operacionais");
--- src/Perfil.java
+++
@@ -1,6 +0,0 @@
-public class Perfil {
-    public static void main(String[] args) {
-        System.out.println("Olá, Vitor!");
-        System.out.println("Tudo bem?");
-    }
-}
