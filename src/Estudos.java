2c2a95a9442d4594d3048fc41a04f2c0fe6284bf
faximon <vitor.faximon@gmail.com>
2024-09-12 19:44:50 -0300

    Desafio da Aula 1

---
+++ src/Estudos.java
@@ -0,0 +1,6 @@
+public class Estudos {
+    public static void main(String[] args) {
+        System.out.println("Hoje tive aula de Java!");
+        System.out.println("Aprendemos sobre as formas que a JDK executa em diferentes sistemas operacionais");
+    }
+}
